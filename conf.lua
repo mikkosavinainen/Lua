c8b3575d4e6a5801cf7d8caf2d509b35f050d6d4
Mikko Savinainen <mj.savinainen@gmail.com>
2014-11-21 09:35:33 +0200

    Title changed

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
-    t.title = "Taalasmaan Seppo"		-- The title of the window the game is in (string)
-    t.author = "ghoulsblade"		-- The author of the game (string)
+    t.title = "Have fun!"	-- The title of the window the game is in (string)
+    t.author = "Mikko Savinainen & Markus Ylinen"		-- The author of the game (string)
     t.screen.width = 1920			-- The window width (number)
     t.screen.height = 1200			-- The window height (number)
     t.screen.fullscreen = false		-- Enable fullscreen (boolean)
